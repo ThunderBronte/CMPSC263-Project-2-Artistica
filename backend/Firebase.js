b0cfa18b81655b64fe02fda325cbee50283fb67b
ThunderBronte <mayachitu@gmail.com>
2025-02-23 22:20:15 -0500

    Initial commit

---
+++ backend/Firebase.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDkhwVjc0_w-Yko1HFgMMRtLcigTR9fGJA",
+//   authDomain: "adoptcatsyippee.firebaseapp.com",
+//   projectId: "adoptcatsyippee",
+//   storageBucket: "adoptcatsyippee.firebasestorage.app",
+//   messagingSenderId: "428727373883",
+//   appId: "1:428727373883:web:5b7c5ff126d28b8cc90c1e",
+//   measurementId: "G-55172PHCZG"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// //const analytics = getAnalytics(app);
+
+// export const storage = getStorage(app);
+// export const database = getFirestore(app);
+// export const analytics = () => getAnalytics(app);
+
+// export default app
