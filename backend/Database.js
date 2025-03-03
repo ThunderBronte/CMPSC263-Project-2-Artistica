8d8d0f7943a629377d91be2aa28c275eec57eae5
ThunderBronte <mayachitu@gmail.com>
2025-03-03 05:23:58 -0500

    database - information

--- backend/Database.js
+++ backend/Database.js
@@ -1,46 +1,26 @@
 import { doc, setDoc, getDoc, getDocs, collection, query, where } from "firebase/firestore"
 import { database } from "./Firebase"
+import { getDatabase, ref, set, onValue } from "firebase/database"
 
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-/*
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBXrqIrp4HG_u-74JksJGzXYB6vTks8CKY",
-  authDomain: "purrfectpals-5f34e.firebaseapp.com",
-  projectId: "purrfectpals-5f34e",
-  storageBucket: "purrfectpals-5f34e.firebasestorage.app",
-  messagingSenderId: "570003181835",
-  appId: "1:570003181835:web:f8e4618b847ab6f490120e",
-  measurementId: "G-F5M2S5MEYE"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-
-// Get a list of cities from the database
-// (example)
-/*
-async function getCities(db){
-    const citiesCol = collection(db, 'cities');
-    const citySnapshot = await getDocs(citiesCol);
-    const cityList = citySnapshot.docs.map(doc => doc.data());
-    return cityList;
+try{
+  const docRef = await addDoc(collection(database, "UserCatCollection"), {
+    userEmail: "email",
+    catInfo: {
+      id: "id",
+      url: "url"
+    }
+  });
+  console.log("doc written w ID: ", docRef.id)
+} catch(err){
+  console.error("Error w adding doc: ", err);
 }
-*/
+
+const querySnapshot = await getDocs(collection(database, "UserCatCollection"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.userEmail} => ${doc.catInfo}`);
+})
 
 
 
@@ -55,15 +35,29 @@ async function getCities(db){
 
 
 
-// const analytics = getAnalytics(app);
 
 
 
-// //Do i need??
-// var admin = require("firebase-admin");
 
-// var serviceAccount = require("path/to/serviceAccountKey.json");
+/*
+// wrong thing...
+function writeToDB(email, catId, url){
+  const reference = ref(getDatabase(), "/userInfo");
+  set(reference, {
+    email: email,
+    catInfo: {
+      id: catId,
+      url: url
+    }
+  });
+}
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
+function readDB(){
+  const catUrlRef = ref(database, "/userInfo/catInfo/url");
+  onValue(catUrlRef, (snapshot) => {
+    const data = snapshot.val();
+    update;
+  })
+}
+
+writeToDB("em@gmail.com", "ididididididid", "url.url.com"); */
--- backend/Firebase.js
+++ backend/Firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 
@@ -11,45 +12,25 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
     apiKey: "AIzaSyBXrqIrp4HG_u-74JksJGzXYB6vTks8CKY",
     authDomain: "purrfectpals-5f34e.firebaseapp.com",
+    databaseURL: "https://purrfectpals-5f34e-default-rtdb.firebaseio.com",
     projectId: "purrfectpals-5f34e",
     storageBucket: "purrfectpals-5f34e.firebasestorage.app",
     messagingSenderId: "570003181835",
-    appId: "1:570003181835:web:f8e4618b847ab6f490120e",
-    measurementId: "G-F5M2S5MEYE"
-};
+    appId: "1:570003181835:web:089a4797516167ea90120e",
+    measurementId: "G-C5545Y96GR"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getFirestore(app);
 
 
-export { app, auth }
+export { app, auth, database }
 
 
 
 /*
-// New users, create account
-createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        const user = UserInformation.UserInformation;
-    })
-    .catch((error) =>{
-        const errorCode = error.code;
-        const errorMessage = error.message;
-    });
-
-
-// Returning Users
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
 
 
 // Do something when the auth changes 
