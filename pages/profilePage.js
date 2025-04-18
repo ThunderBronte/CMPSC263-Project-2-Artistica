852eaca1e38c52a3ff5c93d78bb189e3dd0597a9
ThunderBronte <mayachitu@gmail.com>
2025-04-18 16:06:39 -0400

    finished profile page!

--- pages/profilePage.js
+++ pages/profilePage.js
@@ -68,29 +68,62 @@ export default function Home() {
     <>
     <NavigationBar />
         <ContentContainer>
-          <Header>
-            <Left>
-              <Img src="/profilePicture.png"></Img>
-              <Heading>Hello {name}!</Heading>
-              {/* <Thanks>Thank you for being a user with us!</Thanks> */}
-              <Links>Here are some quick Links:</Links>
-              <ButtonLinks>
-                <Button onClick={() => router.push("/")}>Homepage</Button>
-                <Button onClick={() => router.push("/searchCat")}>Search Cats</Button>
-                <Button onClick={() => router.push("/catCart")}>Cat Cart</Button>
-              </ButtonLinks>
-            </Left>
-            <Right>
-              <Subheading>Here is your information: </Subheading>
-                <ProfileInfo>Current Username: {name}</ProfileInfo>
-                <ProfileInfo>Current email: {email}</ProfileInfo>
-                <ProfileInfo><Button onClick={() => signOutUser()}>Sign Out</Button></ProfileInfo>
-            <Subheading> Want to see your current Cat Cart? </Subheading>
-            <ProfileInfo><Button onClick={() => router.push('/catCart')}>Go to Cat Cart list!</Button></ProfileInfo>
-            </Right>
-            
-          </Header>
-          
+          <Space>.</Space>
+          <TitleScreen>
+            <Title>Hello!</Title>
+            <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+              <PageText><Yellow>I have not set up the forced log in, not entirely sure how to yet. This page will only become accessable if someone logsnin with their MetaMask</Yellow></PageText>
+          </TitleScreen>
+          <TextContent>
+            <SectionContainer>
+              <ProfileContainer>
+                  <ProfileImage src="Images/PersonalProfilePicture.jpg"></ProfileImage>
+                 <PageInfo>
+                    <PageText>Welcome to your Profile Page!</PageText>
+                    <PageText>Here, you can view your art and see completed and uncompleted contracts. </PageText>
+                  </PageInfo>
+              </ProfileContainer>
+                <Subheading> <Yellow>Art you have posted: </Yellow> </Subheading>
+                <SectionContainer>
+                  <AllArtCont>
+                      <ImageContainer>
+                        <Image src="Images/art1.webp"></Image>
+                        <ArtText>Title: Art1</ArtText>
+                      </ImageContainer>
+                      <ImageContainer>
+                        <Image src="Images/art2.jpg"></Image>
+                        <ArtText>Title: Art2</ArtText>
+                      </ImageContainer>
+                  </AllArtCont>
+                </SectionContainer>
+                <Subheading> <Yellow>Current Contracts</Yellow> </Subheading>
+                  <AllArtCont>
+                    <ImageContainer>
+                      <ArtText>Art Trade</ArtText>
+                      <ArtText>Currently With: Artist 6</ArtText>
+                    </ImageContainer>
+                    <ImageContainer>
+                      <ArtText>Commision</ArtText>
+                      <ArtText>Currently With: Artist 8</ArtText>
+                    </ImageContainer>
+                  </AllArtCont>
+                <Subheading> <Yellow>Completed Contracts</Yellow> </Subheading>
+                  <AllArtCont>
+                    <ImageContainer>
+                      <ArtText>Art Trade</ArtText>
+                      <ArtText>Done with: Artist 1</ArtText>
+                    </ImageContainer>
+                    <ImageContainer>
+                      <ArtText>Art Trade</ArtText>
+                      <ArtText>Done with: Artist 2</ArtText>
+                    </ImageContainer>
+                    <ImageContainer>
+                      <ArtText>Commision</ArtText>
+                      <ArtText>Done with: Artist 3</ArtText>
+                    </ImageContainer>
+                  </AllArtCont>
+              </SectionContainer>
+            </TextContent>
         </ContentContainer>
       <Footer />
     </>
@@ -98,14 +131,136 @@ export default function Home() {
 };
 
 
+
 const ContentContainer = styled.div`
-  background-color: white;
-  padding: 20px;
-  color: #25283D;
-  
-  margin-left: 15%;
-  margin-right: 15%;
+background-color: #120E14;
+color: white;
 `;
+
+const Space = styled.div` color: #120E14; `;
+
+const TitleScreen = styled.div`
+  background-color: #120E14;
+  margin-left: 9%;
+  margin-right: 9%;
+
+  overflow: hidden;
+`;
+
+const Title = styled.div`
+  margin = -10px;
+  font-size: 60px;
+  padding: 20px;
+  text-align: left;
+  float: left;
+  display: inline;
+`;
+
+
+const AllArtCont = styled.div`
+  display: grid;
+  justify-content: space-evenly;
+  padding-top: 50px;
+
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-rows: auto; 
+  gap: 5px;
+
+  grid-auto-flow: row;
+`;
+
+const ProfileContainer = styled.div`
+  padding: 10px;
+  overflow: auto;
+`;
+
+const ImageContainer = styled.div`
+  text-align: center;
+  border-radius: 7px;
+  // width: 320px;
+  // height: 380px;
+  overflow: auto;
+
+
+  &:hover {
+    //cursor: pointer;
+  }
+`;
+
+const ProfileImage = styled.img`
+  float: left;
+  height: 275px;
+  width: 275px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-top: 15px;
+`;
+
+const Image = styled.img`
+  height: 275px;
+  width: 275px;
+  border-radius: 5px;
+  object-fit: cover;
+  margin-top: 15px;
+`;
+
+const ArtText = styled.p`
+  font-size: 20px;
+`;
+
+const Yellow = styled.span`  color: #FFD725;`;
+
+const TextContent = styled.div`
+  text-align: center;
+  padding-bottom: 30px;
+`;
+
+const SectionContainer = styled.div`
+  background-color: #120E14;
+  padding-bottom: 5%;  
+  margin-left: 9%;
+  margin-right: 9%;
+`;
+
+const PageInfo = styled.div`
+  float: right;
+  background-color: #120E14;
+  padding-bottom: 2%; 
+  padding-top: 8%;  
+  margin-left: 10%;
+  margin-right: 10%;
+
+`;
+
+const PageText = styled.div`
+  text-align: left;
+  font-size: 20px;
+`;
+
+const Button = styled.button`
+margin-top: 5px;
+font-size: 20px;
+margin: 4%;
+padding: 1%;
+padding-left: 2%;
+padding-right: 2%;
+
+color: black;
+background-color: #FFD725; 
+border: 2px solid #FFD725;
+border-radius: 10px;
+
+&:hover{ 
+  color: black;
+  border-color: #CEAD1B;
+  background-color: #CEAD1B;
+  cursor: pointer;
+}
+`;
+
+
+
+
 
 const Header = styled.div`
   display: grid;
@@ -144,39 +299,5 @@ const Heading = styled.h1`
 
 const Subheading = styled.h2`
   font-size: 30px;
-  padding: 20px;
-  margin-top: 50px;
+  margin-top: 100px;
 `;
-
-const ProfileInfo = styled.p`
-  font-size: 20px;
-  padding-left: 10%;
-  padding-bottom: 10px;
-`;
-
-
-const Button = styled.button`
-  font-size: 20px;
-  padding: 5px;
-  padding-left: 10px;
-  padding-right: 10px;
-  border-radius: 4px;
-  margin: 15px;
-
-  display: flex;
-  gap: 10px;
-
-  color: white;
-  background-color: #077678; 
-  border: 2px solid #077678;
-  border-radius: 50px;
-
-  &:hover{ 
-    color: #077678;
-    border-color: #077678;
-    background-color: transparent;
-    cursor: pointer;
-  }
-
-`;
-
